79033198e7cf48e52c9656f785d3f42554e49a3b
timata <551307910@hcmut.edu.vn>
2015-05-29 15:29:36 +0700

    Small Change.

--- src/v2/org/analysis/Main.java
+++ src/v2/org/analysis/Main.java
@@ -101,11 +101,11 @@ public class Main {
 		pathVirus = "asm/api/";
 		// pathVirus = "asm/APLAS14/";
 		// pathVirus = "asm/virus/";
-		//pathVirus = "asm/packer/";
+		pathVirus = "asm/packer/";
 		// pathVirus = "D:/Temp/Test_week_6/";
 		// pathVirus = "asm/mcvecto/";
 		pathVirus = "asm/vx.netlux.org/";
-		//pathVirus = "C:/Software/Virus/API/";
+		// pathVirus = "C:/Software/Virus/API/";
 		// pathVirus = "asm/WindowsXP/";
 		// pathVirus = "asm/demo1/";
 		// pathVirus = "asm/Test-case/";
@@ -240,38 +240,37 @@ public class Main {
 
 		// SEFM 2015
 		in = "Virus.Win32.Seppuku.3426";
-		in = "Virus.Win32.Seppuku.3291";
-		in = "%Virus.Win32.Savior.1832";
-		in = "%Virus.Win32.Savior.1740";
-		in = "Virus.Win32.Canabas.2999";
-		in = "Virus.Win32.Cabanas.b";
-		in = "Virus.Win32.Cabanas.a";
-		in = "Email-Worm.Win32.Bagle.af";
-		in = "Virus.Win32.Wit.a"; // 16 17 Yen check
-		//in = "Virus.Win32.Htrip.a"; // 71 71 Yen check
-		//in = "Virus.Win32.Eva.a"; // 239 248
-		//in = "Virus.Win32.Cornad"; // 69 74
+		in = "Virus.Win32.Seppuku.3291"; // 61 63
+		//in = "Virus.Win32.Savior.1832";
+		//in = "Virus.Win32.Savior.1740"; // 91 94
+		//in = "Virus.Win32.Canabas.2999"; // 385 404 41s
+		//in = "Virus.Win32.Cabanas.b"; // 385 404 41s
+		//in = "Virus.Win32.Cabanas.a"; //  385 404 41s 
+		//in = "Email-Worm.Win32.Bagle.af"; // loop Vo tan
+		//in = "Virus.Win32.Wit.a";
+		//in = "Virus.Win32.Htrip.a";
+		// in = "Virus.Win32.Eva.a";
+		// in = "Virus.Win32.Cornad";
 		// in = "Virus.Win32.Compan.a"; // 79 87
-		//in = "Virus.Win32.Cerebrus.1482"; // 166 174
+		// in = "Virus.Win32.Cerebrus.1482";
 		// in = "Virus.Win32.Bogus.4096"; // 97 107 45s
 		// in = "Virus.Win32.Brof.a"; // 190 210 4s 
 		// in = "Virus.Win32.Benny.3223"; //210 226 4s
 		// in = "Virus.Win32.Benny.3219.a"; // 210 226 5s
-		// in = "Virus.Win32.Belial.a"; // 130 134 307s Yen
+		// in = "Virus.Win32.Belial.a"; // 86 86 5s Yen
 		// in = "Virus.Win32.Aztec.01"; //243 252 11s
 		// in = "Trojan-PSW.Win32.QQRob.16.d"; // 348 382 2618
 		// in = "Email-Worm.Win32.Coronex.a"; // 219 229 11s
 		// in = "Email-Worm.Win32.Klez.h"; // Header false
-		// in = "Virus.Win32.Artelad.2173"; // 220 228 70 7c817067 - Checked
-		// later
-		// in = "Email-Worm.Win32.Apbost.c"; // 432 462 3
-		// in = "Email-Worm.Win32.LoveLetter.b"; //> 7000
+		//in = "Virus.Win32.Artelad.2173"; // 220 228 70 7c817067 - Checked later
+		//in = "Email-Worm.Win32.Apbost.c"; // 432 462 3
+		//in = "Email-Worm.Win32.LoveLetter.b"; //> 7000 Checked
 		//in = "Virus.Win32.Pulkfer.a"; // 8377 8385 87 Checked later
 
 		// ICFEM
 		// in = "Worm.Win32.Deborm.ah"; // Fail due to MOVS with too high loop
 		// SEH
-		// in = "Virus.Win32.HLLO.Momac.a"; // Fail due to MOVS with too high
+		in = "Virus.Win32.HLLO.Momac.a"; // Fail due to MOVS with too high
 		// loop
 		// in = "Virus.Win32.Donut"; // 4542 4543 truy cap 77e60000
 		// in = "Email-Worm.Win32.Atak.e"; // 5654 5653 2
@@ -287,7 +286,7 @@ public class Main {
 		// in = "Email-Worm.Win32.Apbost.c"; // 317 345 SEH
 		// in = "Email-Worm.Win32.Mydoom.az"; // 952 981 47s
 		// in = "Email-Worm.Win32.Zeynep.j"; // 442 470 317s
-		// in = "Virus.Win32.Cabanas.2999"; // 385 404 45s;
+		//in = "Virus.Win32.Cabanas.2999"; // 385 404 45s;
 
 		// Windows
 		// in = "cmd.exe"; // 484 493 273s
@@ -318,17 +317,17 @@ public class Main {
 
 		// Done
 		// in = "api_test.exe"; // 158 160 0.1s x
-		// in = "api_test_upx.exe"; // 323 353 21s x
+		//in = "api_test_upx.exe"; // 323 353 21s x
 		// in = "api_test_fsg.exe"; // 244 268 5s x
 		// in = "api_test_pecompact.exe"; // 1127 1178 35s x
 		// in = "api_test_npack.exe"; // 602 639 10s x
 		// in = "api_test_yoda.1.2.exe"; // 622 659 80s x
 		// in = "api_test_yoda.1.3.exe"; // 909 945 54s x
 		//in = "api_test_petite_2.3.exe"; // 1569 1637 144s x
-		in = "api_test_aspack.exe"; // 1047 1112 101s x
+		//in = "api_test_aspack.exe"; // 1047 1112 101s x
 
 		// in = "api_test_yoda.exe"; // 962 1038 257s
-		//in = "api_test_v2.3_lvl1.exe"; // 19177 19384 179963
+		// in = "api_test_v2.3_lvl1.exe"; // 19177 19384 179963
 
 		// in = "Virus.Win32.Aztec.01"; // 312 330 Done
 
