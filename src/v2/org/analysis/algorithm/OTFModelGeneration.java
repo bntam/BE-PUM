a773fd46ff12f55d79953766c4355a37244d9e39
doduyphong <doduyphongbktphcm@gmail.com>
2015-08-01 18:56:06 +0700

    - Updated packer detection by tracing file data.

--- src/v2/org/analysis/algorithm/OTFModelGeneration.java
+++ src/v2/org/analysis/algorithm/OTFModelGeneration.java
@@ -106,6 +106,13 @@ public class OTFModelGeneration implements Algorithm {
 		TIB.updateTIB(curState);
 		// ---------------------------------------------
 
+		// PHONG - 20150801 /////////////////////////////
+		// Packer Detection via Header
+		System.out.println("================PACKER DETECTION======================");
+		program.getDetection().detectViaHeader(program);
+		System.out.println("======================================================");
+		/////////////////////////////////////////////////
+		
 		while (!pathList.isEmpty()) {
 			/*
 			 * if (System.currentTimeMillis() - overallStartTime >
@@ -184,10 +191,12 @@ public class OTFModelGeneration implements Algorithm {
 			}
 		}
 		// PHONG - 20150724
+		/*
 		System.out.println("================PACKER DETECTION======================");
 		System.out.println(program.getDetection().getTechniques().getDetailTechniques());
 		program.getDetection().packedBy();
 		System.out.println("======================================================");
+		*/
 	}
 
 	private void backupState(BPState curState) {
--- src/v2/org/analysis/algorithm/PackerDetection.java
+++ src/v2/org/analysis/algorithm/PackerDetection.java
@@ -1,15 +1,13 @@
 package v2.org.analysis.algorithm;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.jakstab.Program;
+
 public class PackerDetection {
-	
-	private static final String UPX 		= "10110000010";
-	private static final String FSG 		= "10111000010";
-	private static final String PECOMPACT 	= "11111111110";
-	private static final String NPACK 		= "10111000010";
-	private static final String PETITE 		= "00110000100";
-	private static final String YODA 		= "10111101111";
-	private static final String ASPACK 		= "10101000000";
-	
+
 	private PackerTechniques techniques;
 	
 	public PackerDetection ()
@@ -21,19 +19,133 @@ public class PackerDetection {
 	{
 		String packedby = "File packed by ";
 		String pTech = techniques.getDetailTechniques();
-		packedby += (pTech.equals(UPX)) 		? "UPX ": "";
-		packedby += (pTech.equals(FSG)) 		? "FSG ": "";
-		packedby += (pTech.equals(PECOMPACT)) 	? "PECOMPACT ": "";
-		packedby += (pTech.equals(NPACK)) 		? "NPACK ": "";
-		packedby += (pTech.equals(PETITE)) 		? "PETITE ": "";
-		packedby += (pTech.equals(YODA)) 		? "YODA ": "";
-		packedby += (pTech.equals(ASPACK)) 		? "ASPACK ": "";
+		packedby += (pTech.equals(PackerConstants.UPX)) 		? "UPX ": "";
+		packedby += (pTech.equals(PackerConstants.FSG)) 		? "FSG ": "";
+		packedby += (pTech.equals(PackerConstants.PECOMPACT)) 	? "PECOMPACT ": "";
+		packedby += (pTech.equals(PackerConstants.NPACK)) 		? "NPACK ": "";
+		packedby += (pTech.equals(PackerConstants.PETITE)) 		? "PETITE ": "";
+		packedby += (pTech.equals(PackerConstants.YODA)) 		? "YODA ": "";
+		packedby += (pTech.equals(PackerConstants.ASPACK)) 		? "ASPACK ": "";
 		System.out.println(packedby);
 	}
 	
 	public PackerTechniques getTechniques ()
 	{
 		return techniques;
+	}
+	
+	public void detectViaHeader (Program prog)
+	{
+		// Entry point
+		String byteEP = String.format("%02x", prog.getByteValueMemory(prog.getEntryPoint()));
+		
+		// Read file
+		String file = prog.getAbsolutePathFile();
+		Path path = Paths.get(file);
+		String[] dataString = null;
+		try {
+			byte[] dataByte = Files.readAllBytes(path);
+			dataString = new String[dataByte.length];
+		    for(int i = 0; i < dataByte.length; i++) {
+		        dataString[i] = String.format("%02x", dataByte[i]);
+		    }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String packedby = "File packed by ";
+		// UPX
+		if (detectWithPacker(dataString, PackerConstants.hUPX, byteEP))
+		{
+			packedby += "UPX";
+			System.out.println(packedby);
+			return;
+		}
+		// FSG
+		if (detectWithPacker(dataString, PackerConstants.hFSG, byteEP))
+		{
+			packedby += "FSG";
+			System.out.println(packedby);
+			return;
+		}
+		// PECOMPACT
+		if (detectWithPacker(dataString, PackerConstants.hPECOMPACT, byteEP))
+		{
+			packedby += "PECOMPACT";
+			System.out.println(packedby);
+			return;
+		}
+		// PETITE
+		if (detectWithPacker(dataString, PackerConstants.hPETITE, byteEP))
+		{
+			packedby += "PETITE";
+			System.out.println(packedby);
+			return;
+		}
+		// YODA
+		if (detectWithPacker(dataString, PackerConstants.hYODA, byteEP))
+		{
+			packedby += "YODA";
+			System.out.println(packedby);
+			return;
+		}
+		// ASPACK
+		if (detectWithPacker(dataString, PackerConstants.hASPACK, byteEP))
+		{
+			packedby += "ASPACK";
+			System.out.println(packedby);
+			return;
+		}
+		//NPACK
+		// Add later
+	}
+	
+	private boolean detectWithPacker (String[] dataString, String[] hPacker, String byteEP)
+	{
+		if (dataString == null)
+			return false;
+		
+		boolean fromEntryPoint = Boolean.parseBoolean(hPacker[hPacker.length - 1]);
+		int beginTracing = 0;
+		if (fromEntryPoint)
+		{
+			for (int i = 0; i < hPacker.length; i++)
+			{
+				if (hPacker[i].toLowerCase().equals(byteEP))
+				{
+					beginTracing = i;
+					break;
+				}
+			}
+		}
+		
+		boolean trace = true;
+		// Begin tracing
+		for (int i = beginTracing; i < dataString.length && trace; i++)
+		{
+			if (dataString[i].equals(hPacker[0].toLowerCase()) 
+				&& dataString[i+1].equals(hPacker[1].toLowerCase()))
+			{
+				for (int j = 0; j < hPacker.length - 1; j++)
+				{
+					if (!hPacker[j].toLowerCase().equals(dataString[i + j]) 
+						&& !hPacker[j].equals("??"))
+					{
+						return false;
+					}
+					if (j == hPacker.length - 2)
+					{
+						trace = false;
+					}
+				}
+			}
+			else if (i == dataString.length - 2)
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 	
 }
