825073cd985b2e9b59f90f47c07c136ccc6eee31
yennlh <yennglhoang@gmail.com>
2015-09-01 00:42:09 +0700

    ** Windows API ** More Windows APIs have been supported.

--- src/v2/org/analysis/apihandle/winapi/kernel32/Kernel32DLL.java
+++ src/v2/org/analysis/apihandle/winapi/kernel32/Kernel32DLL.java
@@ -3436,4 +3436,44 @@ public interface Kernel32DLL extends StdCallLibrary {
 	 *         call GetLastError.
 	 */
 	int GetThreadPriority(/* _In_ */HANDLE hThread);
+
+	/**
+	 * Opens a named file mapping object.
+	 * 
+	 * @param dwDesiredAccess
+	 *            The access to the file mapping object. This access is checked
+	 *            against any security descriptor on the target file mapping
+	 *            object. For a list of values, see File Mapping Security and
+	 *            Access Rights.
+	 * 
+	 * @param bInheritHandle
+	 *            If this parameter is TRUE, a process created by the
+	 *            CreateProcess function can inherit the handle; otherwise, the
+	 *            handle cannot be inherited.
+	 * 
+	 * @param lpName
+	 *            The name of the file mapping object to be opened. If there is
+	 *            an open handle to a file mapping object by this name and the
+	 *            security descriptor on the mapping object does not conflict
+	 *            with the dwDesiredAccess parameter, the open operation
+	 *            succeeds. The name can have a "Global\" or "Local\" prefix to
+	 *            explicitly open an object in the global or session namespace.
+	 *            The remainder of the name can contain any character except the
+	 *            backslash character (\). For more information, see Kernel
+	 *            Object Namespaces. Fast user switching is implemented using
+	 *            Terminal Services sessions. The first user to log on uses
+	 *            session 0, the next user to log on uses session 1, and so on.
+	 *            Kernel object names must follow the guidelines outlined for
+	 *            Terminal Services so that applications can support multiple
+	 *            users.
+	 * 
+	 * @return If the function succeeds, the return value is an open handle to
+	 *         the specified file mapping object. If the function fails, the
+	 *         return value is NULL. To get extended error information, call
+	 *         GetLastError.
+	 */
+	HANDLE OpenFileMapping(
+	/* _In_ */DWORD dwDesiredAccess,
+	/* _In_ */BOOL bInheritHandle,
+	/* _In_ */String lpName);
 }
---
+++ src/v2/org/analysis/apihandle/winapi/msvcrt/functions/_ltoa.java
@@ -0,0 +1,17 @@
+package v2.org.analysis.apihandle.winapi.msvcrt.functions;
+
+import v2.org.analysis.apihandle.winapi.msvcrt.MSVCRTAPI;
+
+public class _ltoa extends MSVCRTAPI {
+
+	public _ltoa() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
